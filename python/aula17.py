8f9e98896fe89900af2ecea187b011101b5b1ad2
Daniela Morais <dani.m@hotmail.co.uk>
2018-10-11 15:17:56 -0300

    Add exercicios de Python

--- python/aula17.py
+++ python/aula17.py
@@ -11,3 +11,39 @@ for index, item in enumerate(items):
          l.append(item)
 
 print(l)
+
+## chave associada ao maior valor num dicionario
+d = {'a': 1, 'b': 3, 'c': 4, 'd': 5, 'e': 6}
+
+maiorValor = 0
+for key, value in d.items():
+    if value > maiorValor:
+        maiorValor = value
+
+for key, value in d.items():
+    if maiorValor == value:
+        print(key)
+
+## criar um dicionario com a contagem de cada elemento numa lista
+d = {}
+l = [2, 2, 3, 2, 2, 6]
+
+for i in l:
+    if d.get(i) is None:
+        d[i] = 1
+    else:
+        atual = d.get(i)
+        d[i] = atual + 1
+
+for key, value in d.items():
+    print(key, value)
+
+## uma lista é sublista de outra?
+a = [1, 2, 3, 4, 5, 6]
+b = [2, 1, 3, 1, 4]
+
+isSublist = False
+if ([x for x in range(len(a)) if a[x:x+len(b)] == b]) != []:
+     isSublist = True
+
+print(isSublist)
